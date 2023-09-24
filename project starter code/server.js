a16c462ecb797913492a867521ccfd4391c4e353
Ismail Al Kamal <ismail@nawatt.com>
2023-09-24 22:18:13 +0300

    added images and endpoint links

--- project starter code/server.js
+++ project starter code/server.js
@@ -59,5 +59,6 @@ app.get("/", async (req, res) => {
 // Start the Server
 app.listen(port, () => {
   console.log(`server running http://localhost:${port}`);
+
   console.log(`press CTRL+C to stop server`);
 });
